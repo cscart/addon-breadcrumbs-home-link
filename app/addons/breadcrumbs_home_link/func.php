374716489e341261a64cbb205bf5d47e2b88988b
Roman <admiralexo@gmail.com>
2016-09-21 18:03:05 +0400

    [~] {@17401} Variable moved in the module settings.

--- app/addons/breadcrumbs_home_link/func.php
+++ app/addons/breadcrumbs_home_link/func.php
@@ -1,10 +1,12 @@
 <?php
 
+use Tygh\Registry;
+
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 function fn_breadcrumbs_home_link_add_breadcrumb(&$lang_value, &$link)
 {
-    $home_link = fn_url('YOUR_LINK');
+    $home_link = fn_url(Registry::get('addons.breadcrumbs_home_link.link'));
 
     Tygh::$app['view']->assign('breadcrumb_home_link', $home_link);
 }
