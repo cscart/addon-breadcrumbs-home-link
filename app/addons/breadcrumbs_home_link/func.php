65c3406b2e4cca4276d2b52796030bc231c12192
Roman Vlasenko <roof1rst@gmail.com>
2016-09-21 11:19:24 +0400

    [~] {@17401} The curly brace is moved to the new line.

--- app/addons/breadcrumbs_home_link/func.php
+++ app/addons/breadcrumbs_home_link/func.php
@@ -2,7 +2,8 @@
 
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
-function fn_breadcrumbs_home_link_add_breadcrumb(&$lang_value, &$link) {
+function fn_breadcrumbs_home_link_add_breadcrumb(&$lang_value, &$link)
+{
     $home_link = fn_url('YOUR_LINK');
 
     Tygh::$app['view']->assign('breadcrumb_home_link', $home_link);
